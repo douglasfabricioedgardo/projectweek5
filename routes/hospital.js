dc311f446d6942d684aa4bf884893815972aa7bc
douglasfabricioedgardo <douglasfabricioedgardo@gmail.com>
2023-11-07 02:54:01 -0600

    aa

--- routes/hospital.js
+++ routes/hospital.js
@@ -5,7 +5,7 @@ const contactsController = require('../controllers/hospital');
 const validation = require('../middleware/validate');
 const security = require('../middleware/authorize.js');
 
-
+router.get('/', security.checkLogin, contactsController.getAll);
 
 router.get('/:id', security.checkLogin, contactsController.getSingle);
 
